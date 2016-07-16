aa8c2a3c880540a6aedd656c9326faeb37ca058a
Kristijan <kristijan.pajtasev@hotmail.com>
2016-07-16 23:40:12 +0100

    files in directory v1

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-module.exports = function() {
+var fs = require("fs");
 
+//C:\Users\Kristijan\Documents\javascript\location-tree
+module.exports = function(p, level) {
+    //return readPath(__dirname);
+    //console.log(path.resolve("../date-helper/"))
+    //return readPath(path.resolve("../date-helper/"));
+    return readContent("../date-helper/")
+    //return readFile("C:\\Users\\Kristijan\\Documents\\javascript");
 }
+
+
+function readContent(location) {
+    var content = fs.readdirSync(location)
+    var concatContent = [];
+    //if(fs.lstatSync(location).isSymbolicLink()) {
+    //    console.log("hello")
+    //} else {
+    //    console.log("none")
+    //}
+    for(var i = 0, length = content.length; i < length; i++) {
+        var item = "/" + content[i];
+        var isDir = fs.lstatSync(location + item).isDirectory();
+        var type = isDir ? "directory" : "file";
+
+        if(isDir) {
+            var subContent = readContent(location + item);
+            concatContent = concatContent.concat(subContent);
+        } else {
+            concatContent.push(location + item);
+        }
+    }
+
+    return concatContent;
+}
+//module.exports = readContent;
